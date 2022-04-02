d3a212d9cb4a99776424ba9b95bea9515c99a790
kaifeng yu <kaifeng.yu625@outlook.com>
2022-04-02 13:19:22 -0400

    change testing to batch testing, v1

--- test/test_funcs.cpp
+++ test/test_funcs.cpp
@@ -185,14 +185,19 @@ void send_transactions(const char * host_name, size_t account_num, size_t sym_nu
 
   while (1) {
    // std::cout << stop_signal << std::endl;
+    int batch_size = 1000;
     try {
       std::stringstream ss;
       std::string id = std::to_string(rand() % account_num);
-      //std::string sym = std::to_string(rand() % sym_num);
-      //std::string lim = std::to_string(rand() % (limit_high - limit_low) + limit_low);
-      //std::string amt = std::to_string(rand() % (amount_high - amount_low) + amount_low);
-      ss << "<transactions id=\"" << id << "\"><order sym=\"" << std::to_string(rand() % sym_num) << "\" amount=\"" << std::to_string(rand() % (amount_high - amount_low) + amount_low) << "\" limit=\"" << std::to_string(rand() % (limit_high - limit_low) + limit_low) << "\"/> </transactions>";
-	    std::string req = ss.str();
+      ss << "<transactions id=\"" << id << "\">";
+      for (int i = 0; i < batch_size; ++i) {
+        //std::string sym = std::to_string(rand() % sym_num);
+        //std::string lim = std::to_string(rand() % (limit_high - limit_low) + limit_low);
+        //std::string amt = std::to_string(rand() % (amount_high - amount_low) + amount_low);
+        ss << "<order sym=\"" << std::to_string(rand() % sym_num) << "\" amount=\"" << std::to_string(rand() % (amount_high - amount_low) + amount_low) << "\" limit=\"" << std::to_string(rand() % (limit_high - limit_low) + limit_low) << "\"/>";
+      }
+      ss << "</transactions>";
+      std::string req = ss.str();
       ss.str("");
       // std::cout << "request:\n" << req << "\n";
       unsigned size = req.size();
@@ -241,27 +246,30 @@ void send_transactions(const char * host_name, size_t account_num, size_t sym_nu
         //std::cerr << "Wrong results from server!\n" << "buf_size=" << buf_size << " xml_len=" << xml_len << std::endl;
         continue;
       }
-      rapidxml::xml_node<>* node = root->first_node("opened");
-      if (node == 0) {
-        // order not secessfuly opend
-        continue;
-      }
-      rapidxml::xml_attribute<> *attr = node->first_attribute("id");
-      if (attr == 0) {
-        //std::cerr << "Wrong results from server: Opened order do not have an id.\n"; 
-        continue;
+      std::vector<std::string> order_id;
+      for (rapidxml::xml_node<>* node = root->first_node("opened"); node != 0; node = node->next_sibling("opened")) {
+        rapidxml::xml_attribute<> *attr = node->first_attribute("id");
+        if (attr == 0) {
+          //std::cerr << "Wrong results from server: Opened order do not have an id.\n"; 
+          continue;
+        }
+        std::string order_id_temp(attr->value());
+        order_id.push_back(order_id_temp);
       }
       doc.clear();
-      std::string order_id(attr->value());
       // 10% chance to cancel order
       if (cancel_rate != 0 && rand() % cancel_rate < 1) {
-        Client client_cancel(host_name, "12345");
         ss.str("");
-        ss << "<transactions id=\"" << id << "\"><cancel id=\"" << order_id << "\"/></transactions>";
+        ss << "<transactions id=\"" << id << "\">";
+        for (size_t i = 0; i < order_id.size(); ++i) {
+          ss  << "<cancel id=\"" << order_id[i] << "\"/>";
+        }
+        ss << "</transactions>";
         std::string cancel_req = ss.str();
         ss.str("");
         //std::cout << "cancel_request:\n" << cancel_req << "\n";
         unsigned cancel_size = cancel_req.size();
+        Client client_cancel(host_name, "12345");
         send(client_cancel.socket_fd, (char*)&cancel_size, sizeof(unsigned), MSG_NOSIGNAL);
         send(client_cancel.socket_fd, cancel_req.c_str(), cancel_req.size(), MSG_NOSIGNAL);
         //std::cout << cancel_req << std::endl;
@@ -288,12 +296,17 @@ void send_transactions(const char * host_name, size_t account_num, size_t sym_nu
       }
       // 50% chance to query order
       if (query_rate != 0 && rand() % query_rate < 1) {
-        Client client_query(host_name, "12345");
         ss.str("");
-        ss << "<transactions id=\"" << id << "\"><query id=\"" << order_id << "\"/></transactions>";
+        ss << "<transactions id=\"" << id << "\">";
+        for (size_t i = 0; i < order_id.size(); ++i) {
+          ss  << "<query id=\"" << order_id[i] << "\"/>";
+        }
+        ss << "</transactions>";
         std::string query_req = ss.str();
+        ss.str("");
         //std::cout << "query_request:\n" << query_req << "\n";
         unsigned query_size = query_req.size();
+        Client client_query(host_name, "12345");
         send(client_query.socket_fd, (char*)&query_size, sizeof(unsigned), MSG_NOSIGNAL);
         send(client_query.socket_fd, query_req.c_str(), query_req.size(), MSG_NOSIGNAL);
         unsigned xml_len = 0;
