bc4b5eb65ba87b2729e862ea81981bfc19a9b1dc
kaifeng-yu16 <90477174+kaifeng-yu16@users.noreply.github.com>
2022-04-02 22:32:41 -0400

    Update test.sql

--- testing/test.sql
+++ testing/test.sql
@@ -1,1 +1,5 @@
-select a.sum + b.sum, c.sum + d.sum from (select sum(balance) from account)c, (select sum(price * amount) from sym_order where amount>0 AND status='open')d,  (select sum(amount) from position)a, (select sum(-amount) from sym_order where amount<0 AND status='open')b;
+select a.sum + b.sum, c.sum + d.sum 
+from (select sum(balance) from account)c, 
+     (select sum(price * amount) from sym_order where amount>0 AND status='open')d,  
+     (select sum(amount) from position)a, 
+     (select sum(-amount) from sym_order where amount<0 AND status='open')b;
